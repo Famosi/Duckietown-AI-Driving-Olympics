b1bb0459ac0c6f2ac162f0d0197f26d0f8a1dd48
Famosi <simone.faggi@yahoo.it>
2020-06-01 13:42:15 +0200

    reshape collected images

--- duckietown_IL/collect_data.py
+++ duckietown_IL/collect_data.py
@@ -1,18 +1,20 @@
 import cv2
-from env import launch_env
-from duckietown_RL/expert import Expert
+import time
+from duckietown_RL.expert import Expert
+from duckietown_RL.gym_duckietown.simulator import Simulator
+from duckietown_RL.env import launch_env
 from _loggers import Logger
 
 env = launch_env()
-logger = Logger(env, log_file='train.log')
 
-DEBUG = True
+EPISODES, STEPS = 400, 256
 
-STEPS = 200
-EPISODES = 5
+logger = Logger(env, log_file=f'train-{int(EPISODES*STEPS/1000)}k.log')
 
 expert = Expert(env=env)
 
+start_time = time.time()
+print(f"[INFO]Starting to get logs for {EPISODES} episodes each {STEPS} steps..")
 # let's collect our samples
 for episode in range(0, EPISODES):
     for step in range(0, STEPS):
@@ -21,13 +23,16 @@ for episode in range(0, EPISODES):
 
         observation, reward, done, info = env.step(action)
 
-        if done:
-            break
+        # Cut the horizon: obs.shape = (480,640,3) --> (300,640,3)
+        observation = observation[150:450, :]
+        # we can resize the image here
+        observation = cv2.resize(observation, (120, 60))
+        # NOTICE: OpenCV changes the order of the channels !!!
+        observation = cv2.cvtColor(observation, cv2.COLOR_BGR2RGB)
 
-        # we may use this to debug our expert.
-        if DEBUG:
-            cv2.imshow('debug', observation)
-            cv2.waitKey(1)
+        if done:
+            print(f"#Episode: {episode}\t | #Step: {step}")
+            break
 
         logger.log(observation, action, reward, done, info)
 
@@ -36,4 +41,7 @@ for episode in range(0, EPISODES):
 
 logger.close()
 env.close()
+
+end_time = time.time()
+print(f"Process finished. It took {(end_time - start_time) / (60*60):.2f} hours!")
 
