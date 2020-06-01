fb7fa2448a967d5707b21eac9f7b091189205ff1
Famosi <simone.faggi@yahoo.it>
2020-06-01 13:42:42 +0200

    env-collect_data

--- duckietown_IL/env.py
+++ duckietown_IL/env.py
@@ -1,4 +1,4 @@
-from gym_duckietown.simulator import Simulator
+from duckietown_RL.gym_duckietown.simulator import Simulator
 
 
 def launch_env():
@@ -6,15 +6,16 @@ def launch_env():
         seed=123,  # random seed
         map_name="zigzag_dists",
         max_steps=500001,  # we don't want the gym to reset itself
+        domain_rand=True,
         camera_width=640,
         camera_height=480,
-        draw_curve=False,
         frame_skip=2,  # at each step, perform the action 2 times
         frame_rate=15,  # 15fps
         accept_start_angle_deg=4,  # @simone start close to straight
         full_transparency=True,
         distortion=True,
-        randomize_maps_on_reset=False,
+        randomize_maps_on_reset=True,
+        draw_curve=False,
         draw_bbox=False,  # top view
         evaluate=False
     )
