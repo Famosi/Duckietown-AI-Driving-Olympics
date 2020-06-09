511fd248fb3afbfdc2182c5eeeac63c5702c3f66
Famosi <simone.faggi@yahoo.it>
2020-06-09 10:46:22 +0200

    macOS-KMP_DUPLICATE_LIB_OK

--- duckietown_il/eval_keras.py
+++ duckietown_il/eval_keras.py
@@ -3,10 +3,14 @@ sys.path.append("../../")
 from duckietown_rl.gym_duckietown.simulator import Simulator
 from keras.models import load_model
 import cv2
+import os
+
+# for macOS
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 env = Simulator(seed=123, map_name="zigzag_dists", max_steps=5000001, domain_rand=True, camera_width=640,
                 camera_height=480, accept_start_angle_deg=4, full_transparency=True, distortion=True,
-                randomize_maps_on_reset=False, draw_curve=False, draw_bbox=False, frame_skip=1, draw_DDPG_features=False)
+                randomize_maps_on_reset=False, draw_curve=False, draw_bbox=False, frame_skip=1)
 
 model = load_model("trained_models/01_NVIDIA.h5")
 
