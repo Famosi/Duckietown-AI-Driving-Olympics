d74e333a28c14214e816cab54d008cad6130be86
Famosi <simone.faggi@yahoo.it>
2020-07-13 14:10:32 +0200

    lr 1e-2

--- duckietown_il/train_ang_disp_keras.py
+++ duckietown_il/train_ang_disp_keras.py
@@ -235,7 +235,7 @@ model = Model(
     outputs=[angle_model, dist_model]
 )
 
-optimizer = Adam(lr=1e-4, decay=1e-4 / EPOCHS)
+optimizer = Adam(lr=1e-2, decay=1e-2 / EPOCHS)
 # losses = {
 #     "angles": "categorical_crossentropy",
 #     "displacement": "categorical_crossentropy",
