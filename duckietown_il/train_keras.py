c4bdb7ddc8b4de4d7c64e24d4df4d8f55324ddb6
Famosi <simone.faggi@yahoo.it>
2020-07-21 14:29:18 +0200

    SGD optimizer

--- duckietown_il/train_keras.py
+++ duckietown_il/train_keras.py
@@ -93,8 +93,8 @@ test_datagen.fit(x_test)
 # model = VGG16_model()
 model = NVIDIA_model()
 # Define the optimizer
-# optimizer = SGD(lr=0.01, momentum=0.001, nesterov=False)
-optimizer = Adam(lr=1e-3, decay=1e-3/EPOCHS)
+optimizer = SGD(lr=0.01, momentum=0.001, nesterov=False)
+# optimizer = Adam(lr=1e-3, decay=1e-3/EPOCHS)
 # Compile the model
 model.compile(optimizer=optimizer,
               loss=MSE,
