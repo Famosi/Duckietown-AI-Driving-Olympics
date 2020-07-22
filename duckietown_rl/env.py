fcd454aacd6118597406c2eca735597cce2d9860
Famosi <simone.faggi@yahoo.it>
2020-07-22 13:15:10 +0200

    predict 4 steps ahead

--- duckietown_rl/env.py
+++ duckietown_rl/env.py
@@ -9,14 +9,14 @@ def launch_env():
         domain_rand=True,
         camera_width=640,
         camera_height=480,
-        frame_skip=2,  # at each step, perform the action 2 times
+        frame_skip=3,  # at each step, perform the action 2 times
         frame_rate=15,  # 15fps
         accept_start_angle_deg=4,  # @simone start close to straight
         full_transparency=True,
         distortion=True,
         randomize_maps_on_reset=True,
         draw_curve=False,
-        draw_bbox=True,  # top view
+        draw_bbox=False,  # top view
         evaluate=False
     )
 
