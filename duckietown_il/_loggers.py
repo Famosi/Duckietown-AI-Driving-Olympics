6d3bf8c923a647474d55bf7b2cc2c475756b9e42
Famosi <simone.faggi@yahoo.it>
2020-07-23 16:41:04 +0200

    don't log points

--- duckietown_il/_loggers.py
+++ duckietown_il/_loggers.py
@@ -13,15 +13,12 @@ class Logger:
         self._multithreaded_recording = ThreadPoolExecutor(4)
         self._recording = []
 
-    def log(self, observation, action, pts_prev, pts_cur, pts_next, reward, done, info):
+    def log(self, observation, action, reward, done, info):
         x, y, z = self.env.cur_pos
         self._recording.append({
             'step': [
                 observation,
                 action,
-                pts_prev,
-                pts_cur,
-                pts_next
             ],
             # this is metadata, you may not use it at all, but it may be helpful for debugging purposes
             'metadata': [
@@ -56,9 +53,6 @@ class Reader:
         end = False
         observations = []
         actions = []
-        pts_prev = []
-        pts_cur = []
-        pts_next = []
 
         info = []
         angles = []
@@ -70,9 +64,6 @@ class Reader:
                     step = entry['step']
                     observations.append(step[0])
                     actions.append(step[1])
-                    # pts_prev.append(step[2])
-                    # pts_cur.append(step[3])
-                    # pts_next.append(step[4])
 
                     metadata = entry['metadata']
                     angles.append(metadata[0][3])
